0dd3a8ff8114e0ba6e60dea6558c4958f30a4c72
justin lee <yuminjustin@163.com>
2019-02-11 15:34:11 +0800

    Update gulpfile.js

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var uglyfly = require('gulp-uglyfly');
+var log = require('fancy-log');
 var cleanCSS = require('gulp-clean-css');
 var config = require('./build/config');
 
 var root = config.build.outputPathName;
 
-gulp.task('replace', function () {
+gulp.task('default', function (done) {
 
     gulp.src(root + '/static/js/*.js')
         .pipe(replace('.catch(', "['catch'](")) // 解决IE不兼容
-        .pipe(uglyfly().on('error', function (e) {
-            console.log(e);
-        }))
+        .pipe(uglyfly())
+        .on('error', function (err) { // 排错
+            log(gutil.colors.red('[Error]'), err.toString());
+        })
         .pipe(gulp.dest(root + '/static/js/'));
 
     gulp.src(root + '/static/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie6'}))
+        .pipe(cleanCSS({
+            compatibility: 'ie6'
+        }))
         .pipe(gulp.dest(root + '/static/css/'));
-})
-
-
-gulp.task('default', ['replace'], function () {
+    done();
     console.log('打包成功.')
 })
